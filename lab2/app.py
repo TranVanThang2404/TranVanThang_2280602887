06913854efcbe685b73c3d30a27917c39eeb97e3
tranthang244@@ <tranthang2442004@gmail.com>
2025-03-13 15:53:21 +0700

    lab2 endd

--- lab2/app.py
+++ lab2/app.py
@@ -14,18 +14,18 @@ def caesar():
 @app.route("/encrypt", methods=['POST'])
 def encrypt():
     text = request.form['inputPlainText']
-    key = int(request.form['key'])
+    key = int(request.form['inputKeyPlain'])
     caesar_cipher = CaesarCipher()
     encrypted_text = caesar_cipher.encrypt_text(text, key)
-    return f"text: {text}<br/>key, key: {key}<br/>encrypted_text: {encrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>encrypted_text: {encrypted_text}"
 
 @app.route("/decrypt", methods=['POST'])
 def decrypt():
     text = request.form['inputCipherText']
-    key = int(request.form['key'])
+    key = int(request.form['inputKeyCipher'])
     caesar_cipher = CaesarCipher()
     decrypted_text = caesar_cipher.decrypt_text(text, key)
-    return f"text: {text}<br/>key, key: {key}<br/>decrypted_text: {decrypted_text}"
+    return f"text: {text}<br/>key: {key}<br/>decrypted_text: {decrypted_text}"
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5050, debug = True)
+    app.run(host='0.0.0.0', port=5050, debug=True)
